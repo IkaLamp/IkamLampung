635b281f3de88aab326534445732e9c3ac4e7082
dindavirguinia <36815554+dindavirguinia@users.noreply.github.com>
2018-11-20 21:31:41 +0700

    Add files via upload

--- application/views/saintek.php
+++ application/views/saintek.php
@@ -1,24 +1,22 @@
 <!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-	<title>Bundle Soal</title>
-
+	<title>Soal_Saintek</title>
 	<!--    <meta name="description" content="">-->
 
     <link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
-      <link rel="stylesheet" href="<?=base_url()?>assets/style/bundle.css">
+      <link rel="stylesheet" href="<?=base_url()?>assets/style/tkpa.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
     <script src="<?=base_url()?>assets/js/animasi.js"></script>
-
 </head>
-
 <body>
 
- <div class="header">
+<div class="header">
                 <div class="cont">
                     <div class="logo">
                         <img src="<?=base_url()?>assets/image/new.jpg"/>
@@ -40,23 +38,55 @@
                 </div>
     </div>
 
-<div class="soal">
-    <div id="tkpa">
-        <a href=""> <H1>TKPA</H1></a>
-    </div>
-    <div id="soshum">
-        <a href="#"><H1>Soshum</H1></a>
-    </div>
-    <div id="saintek">
-        <a href="#"><H1>Saintek</H1></a>
-    </div>
+ 
+<div class="daftar">
+    <div class="table">
+        <H1>Soal Saintek</H1> <br><br>
+            <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Tahun</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2012</td>
+            <td><a href="#"> download</a></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2013</td>
+            <td><a href="#">download</a></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>2014</td>
+            <td><a href="#">download</a></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>2015</td>
+            <td><a href="#">download</a></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>2016</td>
+            <td><a href="#">download</a></td>
+        </tr>
+    </table>
+</body>
 </div>
+
+
+        
+    </div>
+    
+
 
 <div class="footer">
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </div>
-
 
 
 </body>
