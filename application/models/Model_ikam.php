e65f443988f71576217c58f97f74faa5e952c5ef
Rizki Muhammad Farhan <31727398+kihan18@users.noreply.github.com>
2018-11-21 19:04:33 +0700

    update Model_ikam untuk Login

---
+++ application/models/Model_ikam.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Model_ikam extends CI_Model {
+	
+	function getBeasiswa(){
+		return $data = $this->db->get('beasiswa');
+	}
+	function getKampus(){
+		return $data = $this->db->get('kampus');
+	}
+	function ambil($param_id, $id, $table)
+	{
+		return $this->db->get_where($table, array($param_id => $id));
+	}
+}
+?>
