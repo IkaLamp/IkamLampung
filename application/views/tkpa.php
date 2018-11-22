fc737fb4c53ddea8c4ad2a3a9274ae9553e8c43d
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2018-11-23 00:02:40 +0700

    Update tkpa.php

--- application/views/tkpa.php
+++ application/views/tkpa.php
@@ -2,13 +2,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-	<title>Soal_TKPA</title>
-	<!--    <meta name="description" content="">-->
+    <title>Soal_TKPA</title>
+    <!--    <meta name="description" content="">-->
 
     <link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
-      <link rel="stylesheet" href="<?=base_url()?>assets/style/tkpa.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/style/tkpa.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/style/style.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/style/bundle.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
@@ -17,76 +19,66 @@
 <body>
 
 <div class="header">
-                <div class="cont">
-                    <div class="logo">
-                        <img src="<?=base_url()?>assets/image/new.jpg"/>
-                    </div>
-                <div class="nav">
-                    <ul>
-                        <li><a href="#">Beasiswa</a></li>
-                        <li><a href="#">Tentang Kampus</a></li>
-                        <li><a href="#">Bundle Soal</a></li>
-                        <div class="search">
-                            <input  type="text" name="user_query" placeholder="Search.."/>
-                            <a href="#">
-                                <i class="fas fa-search" style="color: coral"></i>
-                            </a>
-                        </div>
-                    </ul>
+    <div class="cont">
+        <div class="logo">
+            <img src="<?=base_url()?>assets/image/new.jpg"/>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="#">Beasiswa</a></li>
+                <li><a href="#">Tentang Kampus</a></li>
+                <li><a href="#">Bundle Soal</a></li>
+                <div class="search">
+                    <input  type="text" name="user_query" placeholder="Search.."/>
+                    <a href="#">
+                        <i class="fas fa-search" style="color: coral"></i>
+                    </a>
+                </div>
+            </ul>
 
-                </div>
-                </div>
+        </div>
     </div>
+</div>
 
- 
+
 <div class="daftar">
     <div class="table">
         <H1>Soal TKPA</H1> <br><br>
-            <table border="1">
-        <tr>
-            <th>No</th>
-            <th>Tahun</th>
-            <th>Keterangan</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>2012</td>
-            <td><a href="#"> download</a></td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>2013</td>
-            <td><a href="#">download</a></td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>2014</td>
-            <td><a href="#">download</a></td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>2015</td>
-            <td><a href="#">download</a></td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>2016</td>
-            <td><a href="#">download</a></td>
-        </tr>
-    </table>
+        <table border="1">
+            <tr>
+                <th>No</th>
+                <th>Tahun</th>
+                <th>Keterangan</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2012</td>
+                <td><a href="#"> Download</a></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>2013</td>
+                <td><a href="#">Download</a></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>2014</td>
+                <td><a href="#">Download</a></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>2015</td>
+                <td><a href="#">Download</a></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>2016</td>
+                <td><a href="#">Download</a></td>
+            </tr>
+        </table>
 </body>
 </div>
 
-
-        
-    </div>
-    
-
-
-<div class="footer">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-</div>
 
 
 </body>
