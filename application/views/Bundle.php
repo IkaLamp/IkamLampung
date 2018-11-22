66b33cf9ecd5817d657423c8f174b1013d4bf601
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2018-11-23 00:16:36 +0700

    Update Bundle.php

--- application/views/Bundle.php
+++ application/views/Bundle.php
@@ -8,21 +8,21 @@
  <?php $this->load->view('user/menu'); ?>
 
 <div class="soal">
-    <div id="tkpa">
-        <a href=""> <H1>TKPA</H1></a>
-    </div>
-    <div id="soshum">
-        <a href="#"><H1>Soshum</H1></a>
-    </div>
-    <div id="saintek">
-        <a href="#"><H1>Saintek</H1></a>
-    </div>
-</div>
+     <div id="tkpa">
+         <a href=""> <H1>TKPA</H1></a>
+     </div>
+     <div id="soshum">
+         <a href="#"><H1>Soshum</H1></a>
+     </div>
+     <div id="saintek">
+         <a href="#"><H1>Saintek</H1></a>
+     </div>
+ </div>
 
-<div class="footer">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-</div>
+ <div class="footer">
+     <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+ </div>
+
 
 
 
