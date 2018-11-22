93350e18e96c1f34d88242d7ab5147b5b69d803c
RahmatFitriadi <rahmatfitriadi70@gmail.com>
2018-11-22 10:14:50 +0700

    Add files via upload

---
+++ kampus.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+
+<!-- 
+  Theme Name: Enlight
+  Theme URL: https://probootstrap.com/enlight-free-education-responsive-bootstrap-website-template
+  Author: ProBootstrap.com
+  Author URL: https://probootstrap.com
+  License: Released for free under the Creative Commons Attribution 3.0 license (probootstrap.com/license)
+-->
+<html lang="en">
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>IkamLampung</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="<?=base_url()?>css/styles-merged.css">
+    <link rel="stylesheet" href="<?=base_url()?>css/style.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>css/custom.css">
+    <link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
+    <link rel="stylesheet" href="<?=base_url()?>assets/style/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <script src="<?=base_url()?>assets/js/animasi.js"></script>
+    <link rel="stylesheet" href="<?=base_url()?>assets/bootstrap/bootstrap.css">
+
+    <link href="<?=base_url()?>assets/style/style.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]--> 
+  </head>
+  <body>
+  <div class="header">
+    <div class="cont">
+        <div class="logo">
+            <a href="#"> <img src="<?=base_url()?>assets/image/new.jpg"/></a>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="#">Beasiswa</a></li>
+                <li><a href="<?=base_url()?>index.php/Tentang_kampus"">Tentang Kampus</a></li>
+                <li><a href="#">Bundle Soal</a></li>
+                <div class="search">
+                    <input  type="text" name="user_query" placeholder="Search.."/>
+                    <a href="#">
+                        <i class="fas fa-search fa-2x" style="color: coral"></i>
+                    </a>
+                </div>
+            </ul>
+
+        </div>
+
+    </div>
+</div>
+      </nav>
+    <!--   <section class="probootstrap-section probootstrap-section-colored">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-left section-heading probootstrap-animate mb0">
+              <h1 class="mb0">Prestasi Marang</h1>
+            </div>
+          </div>
+        </div>
+      </section> -->
+      <section class="probootstrap-section">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+               <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+               <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+                <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+
+            <div class="clearfix visible-md-block"></div>
+            
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+                <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+            
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+               <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+              <a href="#" class="probootstrap-featured-news-box">
+                <figure class="probootstrap-media"><img src="<?=base_url()?>assets/image/UNILA.png" class="img-responsive" width="20000" hight="8000"></figure>
+                <div class="probootstrap-text">
+                  <h3>Tempora consectetur unde nisi</h3>
+                  <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+                  <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+                </div>
+              </a>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+
+          </div>
+        </div>
+      </section>
+<div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+        <div class="col-lg-4">
+            <img class="fas fa-thumbs-up fa-5x" style="color: coral" data-aos="fade-up" alt="Generic placeholder image">
+            <h2>TERPERCAYA</h2>
+            <p>Informarsi yang disediakan di halaman ini 100% benar, nyata dan dapat dipercaya.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="fas fa-check-double fa-5x" style="color: coral" alt="Generic placeholder image">
+            <h2>AKURAT</h2>
+            <p>Informasi yang disediakan di halaman ini benar-benar tepat.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="fas fa-star fa-5x" style="color: coral" alt="Generic placeholder image">
+            <h2>TERBARU</h2>
+            <p>Halaman ini akan menyediakan informasi terbaru, hangat, dan tidak ketinggalan zaman.</p>
+        </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+</div>
+
+<div class="container jdul">
+  <h2> <strong> HOT NEWS</strong></h2>
+    <div class="row mb-3">
+    <div class="col-lg-5">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <h3 class="mb-0">
+                    <a class="text-dark" href="#">SNMPN & SBMPTN 2019</a>
+                </h3>
+                <div class="mb-1 text-muted">1 Nov 18</div>
+                <p class="card-text mb-auto">Tahun 2019, kuota SNMPTN dikurangi dan kuota SBMPTN ditambah</p>
+                <a href="#">Lanjutkan membaca</a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-lg-5">
+            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                <div class="card-body d-flex flex-column align-items-start">
+                   <h3 class="mb-0">
+                        <a class="text-dark" href="#">Daftar Beasiswa 2018</a>
+                    </h3>
+                    <div class="mb-1 text-muted">1 Nov 18</div>
+                    <p class="card-text mb-auto">Daftar lengkap Beasiswa luar negeri maupun dalam negeri 2019</p>
+                    <a href="#">Lanjutkan membaca</a>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+
+<!-- FOOTER -->
+<footer class="container">
+    <p class="float-lg-left"></p>
+    <p>&copy; 2019 IkamLampung&middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+    <!-- END wrapper -->
+    
+
+    <script src="<?=base_url()?>js/scripts.min.js"></script>
+    <script src="<?=base_url()?>js/main.min.js"></script>
+    <script src="<?=base_url()?>js/custom.js"></script>
+
+  </body>
+</html>
