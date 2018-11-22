97294750ccdbc50812d12cf76ed7aadb65bebd30
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2018-11-23 00:32:53 +0700

    Update Bundle.php

--- application/views/Bundle.php
+++ application/views/Bundle.php
@@ -20,9 +20,8 @@
  </div>
 
  <div class="footer">
-     <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+     <p>&copy; 2018 IkamLampung</p>
  </div>
-
 
 
 
