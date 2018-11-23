0e111b9a9994adbd0fd3ce1804642af6c465bfbd
RahmatFitriadi <rahmatfitriadi70@gmail.com>
2018-11-23 13:17:36 +0700

    add Menu view

---
+++ application/views/user/menu.php
@@ -0,0 +1,20 @@
+<div class="header">
+    <div class="cont">
+        <div class="logo">
+            <a href="<?=base_url()?>"> <img src="<?=base_url()?>assets/image/new.jpg"/></a>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="<?php echo base_url('index.php/Beas'); ?>">Beasiswa</a></li>
+                <li><a href="<?=base_url()?>index.php/Kamps">Tentang Kampus</a></li>
+                <li><a href="<?php echo base_url('index.php/Soal');?>">Bundle Soal</a></li>
+                <div class="search">
+                    <input  type="text" name="user_query" placeholder="Search.."/>
+                    <a href="#">
+                        <i class="fas fa-search fa-2x" style="color: coral"></i>
+                    </a>
+                </div>
+            </ul>
+        </div>
+    </div>
+</div>
