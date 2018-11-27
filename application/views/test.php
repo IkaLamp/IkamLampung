3279b3d04c92b3d920cf4b14bd7e8ef4f6edf8dc
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2018-11-27 14:58:55 +0700

    Update test.php
    ngubah slider

--- application/views/test.php
+++ application/views/test.php
@@ -1,4 +1,7 @@
-<?php $this->load->view('head'); ?>
+<?php $this->load->view('head');  ?>
+
+<div class="containerr">
+<main role="main">
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -8,37 +11,29 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="first-slide" src="<?=base_url()?>assets/image/itera2.jpg" alt="First slide">
-            <div class="container">
+            <img  src="<?=base_url()?>assets/image/itera2.jpg" alt="First slide">
                 <div class="carousel-caption">
                     <h1>INSTITUT TEKNOLOGI SUMATERA</h1>
                 </div>
             </div>
-        </div>
         <div class="carousel-item">
             <img class="second-slide" src="<?=base_url()?>assets/image/UNILA.png" alt="Second slide">
-            <div class="container">
                 <div class="carousel-caption">
                     <h1>UNIVERSITAS NEGERI LAMPUNG</h1>
                 </div>
             </div>
-        </div>
         <div class="carousel-item">
             <img class="third-slide" src="<?=base_url()?>assets/image/poltekes.jpg" alt="Third slide">
-            <div class="container">
                 <div class="carousel-caption">
                     <h1>POLITEKNIK KESEHATAN LAMPUNG </h1>
                 </div>
             </div>
-        </div>
         <div class="carousel-item">
             <img class="forth-slide" src="<?=base_url()?>assets/image/polinela.jpg" alt="forth slide">
-            <div class="container">
                 <div class="carousel-caption">
                     <h1>POLITEKNIK NEGERI LAMPUNG</h1>
                 </div>
             </div>
-        </div>
     </div>
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -49,35 +44,35 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-
-<div class="container marketing">
-
+</div>
+</div>
+<div class="containerr">
     <!-- Three columns of text below the carousel -->
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col">
             <img class="fas fa-thumbs-up fa-5x" style="color: coral" data-aos="fade-up" alt="Generic placeholder image">
             <h2>TERPERCAYA</h2>
             <p>Informarsi yang disediakan di halaman ini 100% benar, nyata dan dapat dipercaya.</p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
+        <div class="col">
             <img class="fas fa-check-double fa-5x" style="color: coral" alt="Generic placeholder image">
             <h2>AKURAT</h2>
             <p>Informasi yang disediakan di halaman ini benar-benar tepat.</p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
+        <div class="col">
             <img class="fas fa-star fa-5x" style="color: coral" alt="Generic placeholder image">
             <h2>TERBARU</h2>
             <p>Halaman ini akan menyediakan informasi terbaru, hangat, dan tidak ketinggalan zaman.</p>
         </div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
 </div>
-
+</main>
 <div class="container jdul">
   <h2> <strong> HOT NEWS</strong></h2>
     <div class="row mb-3">
-    <div class="col-lg-5">
-        
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
+    <div class="coll">
+
+
             <div class="card-body d-flex flex-column align-items-start">
                <h3 class="mb-0">
                     <a class="text-dark" href="#">SNMPN & SBMPTN 2019</a>
@@ -88,9 +83,9 @@
             </div>
         </div>
     </div>
+    <div class="row mb-3">
+        <div class="coll">
 
-        <div class="col-lg-5">
-            <div class="card flex-md-row mb-4 h-md-250">
                 <div class="card-body d-flex flex-column align-items-start">
                    <h3 class="mb-0">
                         <a class="text-dark" href="#">Daftar Beasiswa 2018</a>
@@ -108,7 +103,7 @@
 <div class="foo">
         <p>&copy; 2018 IkamLampung</p>
          </div>
-
+</main>
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
