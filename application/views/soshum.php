79ec8719c9a9e9fa772526ba1d8de23d131fd60c
dindavirguinia <36815554+dindavirguinia@users.noreply.github.com>
2018-11-27 16:22:54 +0700

    Update soshum.php

--- application/views/soshum.php
+++ application/views/soshum.php
@@ -55,10 +55,10 @@
     
 
 
-<div class="footer">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-</div>
+<!-- FOOTER -->
+<div class="foo">
+        <p>&copy; 2018 IkamLampung</p>
+         </div>
 
 
 </body>
