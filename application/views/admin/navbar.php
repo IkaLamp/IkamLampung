262a4399165fe6f47e15f31a400887a513788c10
wiliagustianpp <35952693+wiliagustianpp@users.noreply.github.com>
2018-11-27 17:36:55 +0700

    Update navbar.php

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -1,8 +1,25 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">InfoKampusLampung</a>
     <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Sign out</a>
-        </li>
+        <div class="row">
+            <ul class="nav">
+                <li class="nav-item px-3">
+                    <a class="nav-link active" href="<?php echo base_url('cadmin'); ?>">
+                        <span data-feather="home"></span>
+                        Dashboard <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="<?php echo base_url('admin/Beadm'); ?>">Beasiswa</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="<?php echo base_url('admin/Newadm'); ?>">Hot News</a>
+                </li>
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">Sign out</a>
+                </li>
+        </div>
+
     </ul>
+
 </nav>
