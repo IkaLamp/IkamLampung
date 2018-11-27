a9741ed94aa32ea8238ab2d77e6131c8976e2a4f
dindavirguinia <36815554+dindavirguinia@users.noreply.github.com>
2018-11-27 18:07:06 +0700

    Update saintek.php

--- application/views/saintek.php
+++ application/views/saintek.php
@@ -1,20 +1,4 @@
-<!DOCTYPE HTML>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-	<title>Soal_Saintek</title>
-	<!--    <meta name="description" content="">-->
-
-    <link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
-    <link rel="stylesheet" href="<?=base_url()?>assets/style/tkpa.css">
-    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/style/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-    <script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-    <script src="<?=base_url()?>assets/js/animasi.js"></script>
-</head>
+<?php $this->load->view('head');  ?>
 <body>
 
 <div class="header">
