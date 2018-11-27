8dfb92ba6d975b8ddd38a72f104d5e35eada3eb7
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2018-11-27 16:10:47 +0700

    Update menu.php

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -1,28 +1,2 @@
-<div class="container-fluid">
-    <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="<?php echo base_url('cadmin'); ?>">
-                            <span data-feather="home"></span>
-                            Dashboard <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo base_url('admin/Beadm'); ?>">Beasiswa</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo base_url('admin/Newadm'); ?>">Hot News</a>
-                    </li>
-            </div>
-    </div>
-</div>
-            </div>
-        </nav>
-
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-
-        </main>
-    </div>
-</div>
+    </main>
