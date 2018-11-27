45e694cb90ae17d0e59bc32db4179525d6482817
mrucii <mutiararuci97@gmail.com>
2018-11-27 16:43:41 +0700

    Add files via upload

---
+++ application/views/beasiswanext.php
@@ -0,0 +1,19 @@
+<?php $this->load->view('head');  ?>
+
+<div class="apa">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img src="<?=base_url()?>assets/image/itera2.jpg" alt="Card image cap">
+
+    <h1>JUDUL BEASISWA</h1>
+
+    <div class="konten">
+        <p>isi konten aklshdkgaskjdbskajbkhoi</p>
+    </div>
+</div>
+
+</body>
+</html>
