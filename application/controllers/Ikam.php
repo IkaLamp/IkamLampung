03b17993febb70c7c7bd9b53b771bafe473b871d
majidilkhithar <nuralimajids@gmail.com>
2018-11-28 10:58:59 +0700

    Update Ikam.php

--- application/controllers/Ikam.php
+++ application/controllers/Ikam.php
@@ -2,11 +2,7 @@
 
 class Ikam extends CI_Controller {
 	function index(){
-		$this->load->model('model_ikam');
-		$data['beasiswa']=$this->model_ikam->getBeasiswa();
-		$data['kampus']=$this->model_ikam->getKampus();
-		$data['hot_news']=$this->model_ikam->getHotNews();
-		$this->load->view('test',$data);
+		$this->load->view('test');
 	}
 }
 ?>
