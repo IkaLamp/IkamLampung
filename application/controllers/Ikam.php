2e2f8c5efb8273a82105ca7661aee0b7905a58ca
Rizki Muhammad Farhan <31727398+kihan18@users.noreply.github.com>
2018-11-29 21:37:52 +0700

    Update Ikam.php

--- application/controllers/Ikam.php
+++ application/controllers/Ikam.php
@@ -2,7 +2,10 @@
 
 class Ikam extends CI_Controller {
 	function index(){
-		$this->load->view('test');
+		$this->load->model('model_ikam');
+		$data['hot_news']=$this->model_ikam->getHotNews('hot_news');
+		$this->load->view('test',$data);
 	}
+
 }
 ?>
